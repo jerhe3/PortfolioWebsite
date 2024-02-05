7a28e1bcbf1c7fb8931c315d51214ec630cb47af
Jerry He <jerryhe365@gmail.com>
2024-02-05 18:29:59 -0500

    Add description and new styling to portfolio items

--- JerryPortfolio/src/data/portfolio.js
+++ JerryPortfolio/src/data/portfolio.js
@@ -2,24 +2,31 @@ export default [
   {
     title: "Portfolio Website",
     imgUrl: "assets/PortfolioWebsite.png",
+    description: "Minimalist Portfolio Website (P.S. You're on it right now)",
     stack: ["React", "Vite", "Tailwind"],
     link: "https://github.com/jerhe3/PortfolioWebsite",
   },
   {
     title: "ForestFolio",
     imgUrl: "assets/ForestFolio.png",
+    description:
+      "CRUD Contact Manager Application. Developed as part of project for COP 4331. Led front-end development.",
     stack: ["HTML", "CSS", "JavaScript", "Bootstrap", "PHP", "mySQL"],
     link: "https://github.com/tmitchell1234/JJTLPContacts",
   },
   {
     title: "PlanPilot",
     imgUrl: "assets/PlanPilot.png",
+    description:
+      "Intelligent Scheduling Application. Developed with team of 4 at ShellHacks 2023. Led API and Authentication development.",
     stack: ["Flutter", "Firebase", "Google Maps API", "Auth0", "OpenAI API"],
     link: "https://devpost.com/software/planpilot",
   },
   {
     title: "PocketTrade",
     imgUrl: "assets/PocketTrade.png",
+    description:
+      "Peer-to-Peer Trading App. Developed with team of 4 at Hackabull. Led Flutter development.",
     stack: ["Flutter", "Firebase", "Swift"],
     link: "https://devpost.com/software/pocket-trade",
   },
