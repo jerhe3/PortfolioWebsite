1ee1a277fbe1fa754429ed53a8aa158dcc1ed18a
Jerry He <jerryhe365@gmail.com>
2024-02-04 23:37:42 -0500

    Add assets to public to dispaly correctly

--- JerryPortfolio/src/data/portfolio.js
+++ JerryPortfolio/src/data/portfolio.js
@@ -1,25 +1,25 @@
 export default [
   {
     title: "Portfolio Website",
-    imgUrl: "src/assets/PortfolioWebsite.png",
+    imgUrl: "assets/PortfolioWebsite.png",
     stack: ["React", "Vite", "Tailwind"],
     link: "https://github.com/jerhe3/PortfolioWebsite",
   },
   {
     title: "ForestFolio",
-    imgUrl: "src/assets/ForestFolio.png",
+    imgUrl: "assets/ForestFolio.png",
     stack: ["HTML", "CSS", "JavaScript", "Bootstrap", "PHP", "mySQL"],
     link: "https://github.com/tmitchell1234/JJTLPContacts",
   },
   {
     title: "PlanPilot",
-    imgUrl: "src/assets/PlanPilot.png",
+    imgUrl: "assets/PlanPilot.png",
     stack: ["Flutter", "Firebase", "Google Maps API", "Auth0", "OpenAI API"],
     link: "https://devpost.com/software/planpilot",
   },
   {
     title: "PocketTrade",
-    imgUrl: "src/assets/PocketTrade.png",
+    imgUrl: "assets/PocketTrade.png",
     stack: ["Flutter", "Firebase", "Swift"],
     link: "https://devpost.com/software/pocket-trade",
   },
