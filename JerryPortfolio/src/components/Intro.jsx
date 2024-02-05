de16952cd67029948beb1e21f18c9c50340757be
Jerry He <jerryhe365@gmail.com>
2024-02-04 22:28:04 -0500

    Added images for portfolio projects

---
+++ JerryPortfolio/src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Intro() {
+    return (
+        <div className="flex items-center justify-center 
+        flex-col text-center pt-20 pb-6">
+            <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold ">Jerry He</h1>
+            <p className="text-base md:text-xl mbb-3 font-medium">Aspiring Software Engineer, CS Student</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">Welcome to my page :&#41; <br></br> Pardon the dust. Still under construction &#x1F6A7; &#x1F477;</p>
+        </div>
+    )
+}
+
+export default Intro;
---
+++ JerryPortfolio/src/data/timeline.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    year: "Summer 2024",
+    title: "Digital Technology Intern",
+    duration: "Upcoming",
+    details: "Incoming Digital Technology Intern @ GE Vernova Greenville",
+  },
+  {
+    year: "Today",
+    title: "B.S. Computer Science",
+    duration: "Ongoing",
+    details:
+      "Currently pursuing my B.S. of Computer Science at the University of Central Florida",
+  },
+  {
+    year: "2023",
+    title: "Software Intern",
+    duration: "1 year",
+    details:
+      "Landed first internship in January 2023 @ Limbitless Solutions. Help develop team applications in Flutter with a Firebase back-end",
+  },
+  {
+    year: "2019",
+    title: "First Line of Code",
+    duration: "Forever",
+    details:
+      "Wrote my first ever line of code in an intro programming class in HS",
+  },
+];
